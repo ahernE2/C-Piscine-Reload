4c299adc6d89b4f3e02acd2c549f9ec2fbfa0639
Alejandro Hernández Hernández <alejhern@cbr5s3.42barcelona.com>
2024-06-08 00:03:17 +0200

    ejercicio 20 done

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 19:01:27 by alejhern          #+#    #+#             */
+/*   Updated: 2024/06/08 00:02:55 by alejhern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_str_len(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *src, char *dest)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+        {
+		dest[i] = src[i];
+		i++;
+        }
+	return (dest);	
+}
+
+char	*ft_strdup(char *src)
+{
+	int	size;
+	char	*cpy;
+	
+	size = ft_str_len(src) + 1;
+	cpy = (char *)malloc(size * sizeof(char));
+	if (cpy == NULL)
+	{
+		return (cpy);
+	}
+	cpy = ft_strcpy(src, cpy);
+	return (cpy);
+}
+/*
+int main() {
+    // Cadena original
+    char *cadena_original = "Hola, mundo";
+
+    // Duplicar la cadena original usando ft_strdup
+    char *cadena_duplicada = ft_strdup(cadena_original);
+    if (cadena_duplicada != NULL) {
+        // Imprimir la cadena original y la duplicada
+        printf("Cadena original: %s\n", cadena_original);
+        printf("Cadena duplicada: %s\n", cadena_duplicada);
+
+        // Liberar la memoria asignada para la cadena duplicada
+        free(cadena_duplicada);
+    } else {
+        // Manejar el caso donde la duplicación falló
+        printf("%s", "Error: No se pudo duplicar la cadena\n");
+    }
+
+    return 0;
+}
+*/
