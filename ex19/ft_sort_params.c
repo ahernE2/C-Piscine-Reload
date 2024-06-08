38ceddaa48ebeef5eaa4a875d821462a2d5dafbc
Alejandro Hernández Hernández <alejhern@car8s6.42barcelona.com>
2024-06-08 16:21:14 +0200

    he sacado la libreria unistd.h

--- ex19/ft_sort_params.c
+++ ex19/ft_sort_params.c
@@ -6,11 +6,9 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 22:06:22 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/08 02:49:43 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/06/08 16:19:44 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <unistd.h>
 
 void	ft_putchar(char c);
 
