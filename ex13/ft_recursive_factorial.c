eafc59433979a9c758fa11eb24789d9884b96a2c
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-06-10 22:49:11 +0200

    ultima correccion

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 21:31:24 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/10 22:15:46 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/06/10 22:44:28 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@ int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0 || nb > 12)
 		return (0);
-	if (nb == 1)
+	if (nb < 1)
 		return (1);
-	return (nb * ft_recursive_factorial(nb -1));
+	else
+		return (nb * ft_recursive_factorial(nb -1));
 }
