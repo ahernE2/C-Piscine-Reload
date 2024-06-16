938085edc834ce36cd1082b48c6b9d11d3a82c47
Alejandro Hernández Hernández <alejhern@student.42barcelona.com>
2024-06-16 20:29:43 +0200

    mejora de lecrura de codigo

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: alejhern <alejhern@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 21:31:24 by alejhern          #+#    #+#             */
-/*   Updated: 2024/06/10 22:44:28 by alejhern         ###   ########.fr       */
+/*   Updated: 2024/06/16 20:28:52 by alejhern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ int	ft_recursive_factorial(int nb)
 {
 	if (nb < 0 || nb > 12)
 		return (0);
-	if (nb < 1)
+	else if (nb < 1)
 		return (1);
 	else
 		return (nb * ft_recursive_factorial(nb -1));
